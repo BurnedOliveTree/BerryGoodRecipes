d47de868b012ce451bd9fa9383e9d679e17c994c
Rolkarolka <romanowskarolina@gmail.com>
2020-11-22 01:39:26 +0100

    typos correction

---
+++ src/main/recipeModel/Recipe.java
@@ -0,0 +1,14 @@
+package main.recipeModel;
+
+public class Recipe {
+    // @TODO private User
+    // @TODO private Ingredients
+    private double rate;
+    private int cost;
+    private String accessibility; // from enum? maybe bool
+    private int portionNumber;
+    private String prepareMethod;
+    private int prepareTime; // in minutes
+
+
+}
