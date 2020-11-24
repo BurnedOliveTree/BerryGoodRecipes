3327d39b06e74ebbd9cc461773d67f6346e98ff7
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2020-11-24 21:12:54 +0100

    implemented basic User class methods

---
+++ src/main/userModel/User.java
@@ -0,0 +1,57 @@
+package main.userModel;
+
+import main.recipeModel.Recipe;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.HashMap;
+
+public class User {
+    private final int ID;
+    private String username;
+    private String password;
+    private List<Recipe> userRecipes;
+    private Map<Recipe, String> favorites;
+    private List<User> followed;
+    // TODO private UnitSystem defaultUnitSystem;
+    private ShoppingList shoppingList;
+
+    public User(int argID, String argUsername, String argPassword) {
+        ID = argID;
+        username = argUsername;
+        password = argPassword;
+        userRecipes = new LinkedList<>();
+        favorites = new HashMap<>();
+        followed = new LinkedList<>();
+        // TODO defaultUnitSystem = argUnitSystem;
+        shoppingList = new ShoppingList();
+    }
+    public void setNewUsername(String newUsername) {
+        username = newUsername;
+    }
+    public void setNewPassword(String newPassword) {
+        password = newPassword;
+    }
+    public void addRecipe(Recipe newRecipe) {
+        userRecipes.add(newRecipe);
+    }
+    public void removeRecipe(Recipe oldRecipe) {
+        userRecipes.remove(oldRecipe);
+    }
+    public void addFavorite(Recipe newFavRecipe) {
+        favorites.put(newFavRecipe, "");
+    }
+    public void removeFavorite(Recipe oldFavRecipe) {
+        favorites.remove(oldFavRecipe);
+    }
+    public void followUser(User newFollowedUser) {
+        followed.add(newFollowedUser);
+    }
+    public void unfollowUser(User oldFollowedUser) {
+        followed.remove(oldFollowedUser);
+    }
+    public void setShoppingList() {
+        // TODO shoppingList.edit();
+    }
+}
