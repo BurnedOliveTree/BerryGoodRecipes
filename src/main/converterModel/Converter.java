df265dfedaf2f22d7a232ffe36e48ed2f5be2103
Marianna Gromadzka <m.gromadzka@stud.elka.pw.edu.pl>
2020-11-25 22:29:05 +0100

    opinion

---
+++ src/main/converterModel/Converter.java
@@ -0,0 +1,5 @@
+package main.converterModel;
+
+public class Converter {
+
+}
---
+++ src/main/userModel/Opinion.java
@@ -0,0 +1,21 @@
+package main.userModel;
+
+
+
+
+public class Opinion {
+    User user;
+    int score;
+    String opinionText;
+
+    public Opinion(String comment, int score, User user){
+        this.opinionText = comment;
+        this.score = score;
+        this.user = user;
+    }
+
+    private void setOpinionText(String text)  {this.opinionText = text;} // do edytowania komentarzy
+
+    private void setScore(int score) {this.score = score;} // do edytowania oceny
+
+}
