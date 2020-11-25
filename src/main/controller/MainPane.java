8d451f7da29cb700b2fc02706ec62289cbefc973
Rolkarolka <romanowskarolina@gmail.com>
2020-11-25 18:54:38 +0100

    Adding recipe view and controlls

--- src/main/controller/MainPane.java
+++ src/main/controller/MainPane.java
@@ -8,8 +8,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import main.recipeModel.Ingredient;
+import main.recipeModel.Recipe;
+import main.recipeModel.RecipeFx;
+import main.recipeModel.Unit;
+import main.userModel.User;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainPane {
     @FXML
@@ -20,13 +26,22 @@ public class MainPane {
     void initialize() {recipeLink.setText("Placki");}
     // Change Scene to RecipeScene
     @FXML
-    public void onClickButton(ActionEvent actionEvent) throws IOException {
-        Parent recipePage = FXMLLoader.load(getClass().getResource("/resources/recipePage.fxml"));
-        Scene recipePageScene = new Scene(recipePage);
-        Stage stage = (Stage) recipeLink.getScene().getWindow();
-        recipePageScene.getStylesheets().add(getClass().getResource("/resources/darkTheme.css").toExternalForm());
-        stage.setScene(recipePageScene);
-        stage.show();
-    }
+    public void onClickButton(ActionEvent actionEvent){
+//        Stage stage = (Stage) recipeLink.getScene().getWindow();
+//        stage.close();
+        try {
+            FXMLLoader loader =  new FXMLLoader(getClass().getResource("/resources/recipePage.fxml"));
+            RecipeFx controller = new RecipeFx(new Recipe("Placki", new User(3, "Karolina", "1234"), "Zrób farsz i nagrzej patelnie", true, new ArrayList<Ingredient>(){{add(new Ingredient(200, new Unit(), "Twaróg"));}}));
+            loader.setController(controller);
+            Parent recipePage = loader.load();
+            Scene recipePageScene = new Scene(recipePage);
+            Stage stage = (Stage) recipeLink.getScene().getWindow();
+            recipePageScene.getStylesheets().add(getClass().getResource("/resources/darkTheme.css").toExternalForm());
+            stage.setScene(recipePageScene);
+            stage.show();
 
+
+        } catch (IOException e) {
+            System.err.println(String.format("Error: %s", e.getMessage()));
+        }}
 }
--- src/main/recipeModel/RecipeFx.java
+++ src/main/recipeModel/RecipeFx.java
@@ -1,26 +1,62 @@
 package main.recipeModel;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+
+import javafx.event.EventHandler;
 import java.io.IOException;
 
 public class RecipeFx {
+    private Recipe recipe;
     @FXML
+    public Button timepieceButton;
+    public Button scaleButton;
+    public TextField numPortionField;
+    public ListView ingredientListView;
+    public Label titleLabel;
+    public Label timeLabel;
+    public Label costLabel;
+    public Button shoppingListButton;
     public Button exitButton;
-    @FXML
-    void initialize() {}
-    @FXML
-    public void onExitButton() throws IOException {
-        Parent mainPage = FXMLLoader.load(getClass().getResource("/resources/mainPage.fxml"));
-        Scene mainPageScene = new Scene(mainPage);
-        Stage stage = (Stage) exitButton.getScene().getWindow();
-        mainPageScene.getStylesheets().add(getClass().getResource("/resources/darkTheme.css").toExternalForm());
-        stage.setScene(mainPageScene);
-        stage.show();
+    public Label descLabel;
+
+    public RecipeFx(Recipe recipe) {
+        this.recipe = recipe;
     }
+
+    @FXML
+    void initialize() {
+        descLabel.setText(this.recipe.getPrepareMethod());
+        titleLabel.setText(this.recipe.getName());
+        timeLabel.setText(String.valueOf(this.recipe.getPrepareTime()));
+        costLabel.setText(String.valueOf(this.recipe.getCost()));
+        numPortionField.setText(String.valueOf(this.recipe.getPortionNumber()));
+        
+        EventHandler<ActionEvent> handler = new EventHandler<>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    Parent mainPage = FXMLLoader.load(getClass().getResource("/resources/mainPage.fxml"));
+                    Scene mainPageScene = new Scene(mainPage);
+                    Stage stage = (Stage) exitButton.getScene().getWindow();
+                    mainPageScene.getStylesheets().add(getClass().getResource("/resources/darkTheme.css").toExternalForm());
+                    stage.setScene(mainPageScene);
+                    stage.show();
+                } catch (IOException e) {
+                    System.err.println(String.format("Error: %s", e.getMessage()));
+                }
+            }
+        };
+        exitButton.addEventHandler(ActionEvent.ACTION, handler);
+    }
+
 }
