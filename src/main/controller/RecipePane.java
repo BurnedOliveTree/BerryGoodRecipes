f9bfbf9340f54d865bf362d5b692c14c3c4279a4
Karolina Romanowska <romanowskarolina@gmail.com>
2020-12-30 16:54:51 +0100

    Delete redundancy

--- src/main/controller/RecipePane.java
+++ src/main/controller/RecipePane.java
@@ -87,12 +87,12 @@ public class RecipePane {
         } else {
             timePrepLabel.setText("Preparation time: " + this.recipe.getPrepareTime());
         }
-        exitButton.setOnAction( e->{ onAction(exitButton, "/resources/mainPage.fxml"); });
-        scaleButton.setOnAction(e->{onAction(scaleButton, "/resources/scalePage.fxml");});
-        shoppingListButton.setOnAction( e->{ onAction(shoppingListButton, "/resources/shoppingListPage.fxml"); });
-        timeButton.setOnAction( e->{ onAction(timeButton, "/resources/timepiecePage.fxml"); });
-        likeButton.setOnAction( e->{ onAction(likeButton, null); });
-        commentButton.setOnAction( e->{ onAction(commentButton, "/resources/opinionPage.fxml"); });
+        exitButton.setOnAction( e->{ onExitAction(exitButton, "/resources/mainPage.fxml"); });
+        scaleButton.setOnAction(e->{onScaleAction(scaleButton, "/resources/scalePage.fxml");});
+//        shoppingListButton.setOnAction( e->{ onShoppingAction(shoppingListButton, "/resources/shoppingListPage.fxml"); });
+//        timeButton.setOnAction( e->{ onTimeAction(timeButton, "/resources/timepiecePage.fxml"); });
+        likeButton.setOnAction( e->{ onLikeAction(likeButton); });
+        commentButton.setOnAction( e->{ onCommentAction(commentButton, "/resources/opinionPage.fxml"); });
         portionArea.textProperty().addListener((observableValue, s, t1) -> {
             try {
                 Double currNumPortions = Double.parseDouble(t1);
@@ -129,69 +129,49 @@ public class RecipePane {
         }
     }
 
-    private void onAction(Button button, String namePath) {
-        if (button.getId().equals("likeButton"))
-            try {
-                LikePic.setImage(new Image(new FileInputStream("src/resources/favoriteClicked.png")));
-            } catch ( FileNotFoundException e) {
-                System.err.println(String.format("Error: %s", e.getMessage()));
-            }
-        else if (button.getId().equals("commentButton")){
-            try {
-                FXMLLoader loader =  new FXMLLoader(getClass().getResource(namePath));
-                OpinionPane controller = new OpinionPane(this.recipe, activeUser);
-                loader.setController(controller);
-                Parent mainPage = loader.load();
-                Scene mainPageScene = new Scene(mainPage);
-                Stage stage = (Stage) button.getScene().getWindow();
-                mainPageScene.getStylesheets().add(getClass().getResource("/resources/"+DatabaseConnection.theme+".css").toExternalForm());
-                stage.setScene(mainPageScene);
-                stage.show();
-            } catch (IOException e) {
-                System.err.println(String.format("Error: %s", e.getMessage()));}
+    private void onLikeAction(Button button) {
+        try {
+            LikePic.setImage(new Image(new FileInputStream("src/resources/favoriteClicked.png")));
+        } catch ( FileNotFoundException e) {
+            System.err.println(String.format("Error: %s", e.getMessage()));
+        }
+    }
 
-        }
-        else if (button.getId().equals("scaleButton")) {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource(namePath));
-                ScalePane controller = new ScalePane(this.recipe, activeUser);
-                loader.setController(controller);
-                Parent mainPage = loader.load();
-                Scene mainPageScene = new Scene(mainPage);
-                Stage stage = (Stage) button.getScene().getWindow();
-                mainPageScene.getStylesheets().add(getClass().getResource("/resources/" + DatabaseConnection.theme + ".css").toExternalForm());
-                stage.setScene(mainPageScene);
-                stage.show();
-            } catch (IOException e) {
-                System.err.println(String.format("Error: %s", e.getMessage()));
-            }
-        }
-        else if (button.getId().equals("exitButton")) {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource(namePath));
-                MainPane controller = new MainPane(activeUser);
-                loader.setController(controller);
-                Scene scene = new Scene(loader.load());
-                Stage stage = (Stage) button.getScene().getWindow();
-                scene.getStylesheets().add(getClass().getResource("/resources/" + DatabaseConnection.theme + ".css").toExternalForm());
-                stage.setScene(scene);
-                stage.show();
-            } catch (IOException e) {
-                System.err.println(String.format("Error: %s", e.getMessage()));
-            }
-        }
-        else {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource(namePath));
-                Scene mainPageScene = new Scene(loader.load());
-                Stage stage = (Stage) button.getScene().getWindow();
-                mainPageScene.getStylesheets().add(getClass().getResource("/resources/"+DatabaseConnection.theme+".css").toExternalForm());
-                stage.setScene(mainPageScene);
-                stage.show();
-            } catch (IOException e) {
-                System.err.println(String.format("Error: %s", e.getMessage()));}
-        }
 
+    private void onCommentAction(Button button, String namePath){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(namePath));
+        ScalePane controller = new ScalePane(this.recipe, activeUser);
+        loader.setController(controller);
+        changeScene(button, loader);
+    }
+
+    private void onScaleAction(Button button, String namePath){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(namePath));
+        ScalePane controller = new ScalePane(this.recipe, activeUser);
+        loader.setController(controller);
+        changeScene(button, loader);
+    }
+
+    private void onExitAction(Button button, String namePath){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(namePath));
+        ScalePane controller = new ScalePane(this.recipe, activeUser);
+        loader.setController(controller);
+        changeScene(button, loader);
+    }
+
+
+
+    private void changeScene(Button button,FXMLLoader loader) {
+        try {
+
+            Parent mainPage = loader.load();
+            Scene mainPageScene = new Scene(mainPage);
+            Stage stage = (Stage) button.getScene().getWindow();
+            mainPageScene.getStylesheets().add(getClass().getResource("/resources/"+DatabaseConnection.theme+".css").toExternalForm());
+            stage.setScene(mainPageScene);
+            stage.show();
+        } catch (IOException e) {
+            System.err.println(String.format("Error: %s", e.getMessage()));}
     }
 
 }
