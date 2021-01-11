be05c609c5d39b8f892ccc0b5416a3a3787a0d6f
Marianna Gromadzka <mgromadz00@gmail.com>
2021-01-11 01:41:40 +0100

    unit function

--- database/procedures.sql
+++ database/procedures.sql
@@ -14,3 +14,17 @@ create or replace trigger tg_add_to_public
 begin
     insert into BELONG values (null, 0, :new.USERNAME);
 end;
+/
+create or replace function convert_unit(first_unit varchar, second_unit varchar, quantity number)
+    return number
+as 
+    new_quantity number(9,5);
+    first_ratio number(9,5);
+    second_ratio number(9,5);
+begin
+    select liter_per_unit_ratio into first_ratio from unit where name = first_unit;
+    select liter_per_unit_ratio into second_ratio from unit where name = second_unit;
+    return (first_ratio*quantity)/second_ratio;
+end;
+/
+
