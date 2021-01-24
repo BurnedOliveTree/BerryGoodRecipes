b5e40cd7e3033ea6028333f700697e174fbe3449
Karolina Romanowska <romanowskarolina@gmail.com>
2021-01-24 02:53:37 +0100

    -value ingredient

--- src/main/controller/ShoppingListPane.java
+++ src/main/controller/ShoppingListPane.java
@@ -98,7 +98,7 @@ public class ShoppingListPane extends BasicPaneActions {
         unit.setPrefWidth(newIngredient.getPrefWidth());
         addButton.setPrefWidth(newIngredient.getPrefWidth());
         addButton.setOnAction(actionEvent -> {
-            if (!quantity.getText().equals("") && !name.getText().equals("")) {
+            if (!quantity.getText().equals("")  && quantity.getText().matches("\\d+(\\.\\d+)?") && !name.getText().equals("")) {
                 Ingredient ingredient = null;
                 try {
                     ingredient = new Ingredient(null, DatabaseConnection.convertUnit(Double.parseDouble(quantity.getText()), unit.getSelectionModel().getSelectedItem().toString(), "gram"),"gram", name.getText());
@@ -123,6 +123,8 @@ public class ShoppingListPane extends BasicPaneActions {
                 } catch (IOException | SQLException e) {
                     e.printStackTrace();
                 }
+            } else {
+                quantity.clear();
             }
         });
         newIngredient.getChildren().addAll(quantity,unit, name, addButton);
